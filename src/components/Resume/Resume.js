c6f69daec286e5581e3034c3c399378ebe35edfb
rishabh gupta <rishabh.katroliya999@gmail.com>
2023-09-03 23:14:26 +0530

    updated resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,29 +1,31 @@
 import Rishabh_Resume from "./Rishabh_Gupta_Resume.pdf";
-import styled from "styled-components"
+import styled from "styled-components";
 import { Button } from "./ResumeElements";
 export const Resume2 = () => {
-    const openResume = () => {
-        // window.open(
+  const openResume = () => {
+     // window.open(
         //   "https://drive.google.com/file/d/1IXncN2l--by_B3-ANHbsHAIRIiEARqKy/view"
         // );
-      }
+  };
 
-      return (
-        <div>
-          <a
-            href={Rishabh_Resume}
-            download="Rishabh_Gupta_Resume"
-          >
-            <Button
-            id="resume-button-2"
-             // onClick={openResume}
-              className="btn btn-md
+  return (
+    <div>
+      <a
+       href={Rishabh_Resume}
+       download="Rishabh_Gupta_Resume"
+        className="nav-link resume"
+        id="resume-button-1 resume-link-1"
+      >
+        <Button
+          onClick={openResume}
+          id="resume-button-1 resume-link-1"
+          className="btn btn-md
                     bg-accent hover:bg-accent-hover 
-                    transition-all md:btn-lg"
-            >
-              Resume
-            </Button>
-          </a>
-        </div>
-      );
-}
+                    transition-all md:btn-lg nav-link resume"
+        >
+          Resume
+        </Button>
+      </a>
+    </div>
+  );
+};
